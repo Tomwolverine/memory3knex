a70e607ff39bc48d14fb7a9a5b082eb564278ddf
tom lee <tdroid99@gmail.com>
2018-08-09 08:50:37 -0600

    made seed file and named table

---
+++ seeds/00_game.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('game').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('game').insert([
+        {id: 1, colName: 'rowValue1'},
+        {id: 2, colName: 'rowValue2'},
+        {id: 3, colName: 'rowValue3'}
+      ]);
+    });
+};
